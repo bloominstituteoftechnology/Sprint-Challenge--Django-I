e10ca27a515b510252c8e531e7625b775ddc30dd
Erik Alonso <mr.alonsoea@protonmail.ch>
2018-06-29 15:05:51 -0500

    .env wip

---
+++ DeploymentExperiences.md
@@ -0,0 +1,8 @@
+## Dependencies that were installed
+
+- pipenv install ...
+- gunicorn
+- psycopg2-binary
+- dj-database-url
+- python-decouple
+- whitenoise
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -118,3 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+from decouple import config
+import dj_database_url
+
+DATABASE_URL = config('DATABASE_URL')
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG')
