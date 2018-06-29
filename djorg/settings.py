1b1c9e8dd47aab13e181cd06ee3123f8a8c29648
Carlos Andrade <carlos.andrade86@outlook.com>
2018-06-29 13:19:35 -0400

    wip4

--- djorg/settings.py
+++ djorg/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['https://cryptic-crag-26999.herokuapp.com/']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -140,4 +140,4 @@ REST_FRAMEWORK = {
     ),
 }
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
