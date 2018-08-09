23dc2160ab0d7ae85704cc7dcb16cd5bb1e0c8cb
Abraham Bueno <abueno@localhost.localdomain>
2018-08-09 12:37:51 -0500

    Changed schema

--- notes/schema.py
+++ notes/schema.py
@@ -3,21 +3,44 @@ import graphene
 from .models import PersonalNote as PersonalNoteModel
 
 class PersonalNote(DjangoObjectType):
+
     class Meta:
         model = PersonalNoteModel
-        #Describing the data as a node in the graph
+        # Describe the data as a node in the graph
         interfaces = (graphene.relay.Node,)
 
 class Query(graphene.ObjectType):
     personalnotes = graphene.List(PersonalNote)
 
-    def resolve_notes(self, info):
+    def resolve_personalnotes(self, info):
         """Decide which notes to use"""
         user = info.context.user # Find with debugger
 
         if user.is_anonymous:
             return PersonalNoteModel.objects.none()
         else:
-            return  PersonalNoteModel.objects.filter(user=user)
+            return PersonalNoteModel.objects.filter(user=user)
 
-schema = graphene.Schema(query=Query)
+class CreatePersonalNote(graphene.Mutation):
+    class Arguments:
+        title = graphene.String()
+        content = graphene.String()
+
+    personalnote = graphene.Field(PersonalNote)
+    ok = graphene.Boolean()
+    status = graphene.String()
+
+    def mutate(self, info, title, content):
+        user = info.context.user
+
+        if user.is_anonymous:
+            return CreatePersonalNote(ok=False)
+        else:
+            new_note = PersonalNoteModel(title=title, content=content, user=user)
+            new_note.save()
+            return CreatePersonalNote(personalnote=new_note, ok=True, status="ok")
+
+class Mutation(graphene.ObjectType):
+    create_personal_note = CreatePersonalNote.Field()
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
