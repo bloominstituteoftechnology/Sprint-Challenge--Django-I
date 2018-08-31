db4e4cf813894dd44cd77cd4792eb4259f35be62
masontmorrow <masontmorrow@gmail.com>
2018-08-31 18:56:34 +0200

    Adjust allowed_hosts

--- djorg/settings.py
+++ djorg/settings.py
@@ -28,9 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [
-    'https://notes-django.herokuapp.com/'
-]
+ALLOWED_HOSTS = config('ALLOWED_HOSTS')
 
 
 # Application definition
