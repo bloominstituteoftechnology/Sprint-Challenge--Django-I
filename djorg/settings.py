93021dfef6e8028ad0178b33b5dd2b9bdd91f05b
masontmorrow <masontmorrow@gmail.com>
2018-08-31 19:50:03 +0200

    static file hell

--- djorg/settings.py
+++ djorg/settings.py
@@ -124,7 +124,12 @@ CORS_ALLOW_CREDENTIALS = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 from rest_framework.authentication import TokenAuthentication, BasicAuthentication, SessionAuthentication
 
