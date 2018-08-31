f9108fc2a883e734458ffcb76b8f67ce808fdaad
Chris Honda <christopher.honda92@gmail.com>
2018-08-30 20:56:48 -0700

    Debugg

--- djorg/settings.py
+++ djorg/settings.py
@@ -27,7 +27,6 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-#Test for .env change
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')])
 
 # Application definition
