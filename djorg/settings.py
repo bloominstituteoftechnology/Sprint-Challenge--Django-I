573f906a29ac0bd90812d359807b1b884d3d44b3
masontmorrow <masontmorrow@gmail.com>
2018-09-04 13:42:21 +0200

    Adjust settings.py

--- djorg/settings.py
+++ djorg/settings.py
@@ -126,9 +126,9 @@ CORS_ALLOW_CREDENTIALS = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 from rest_framework.authentication import TokenAuthentication, BasicAuthentication, SessionAuthentication
 
