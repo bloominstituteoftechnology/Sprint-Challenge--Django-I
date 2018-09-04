a1f97ea44fce30a7bdf4d100a4e210bc5dd23b55
masontmorrow <masontmorrow@gmail.com>
2018-09-04 13:56:33 +0200

    Adjust settings.py

--- djorg/settings.py
+++ djorg/settings.py
@@ -125,7 +125,7 @@ CORS_ALLOW_CREDENTIALS = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # STATICFILES_DIRS = (
 #     os.path.join(BASE_DIR, 'static'),
 # )
