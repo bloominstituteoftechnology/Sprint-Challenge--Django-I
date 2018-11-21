ea1be5a0ced104b3e0cd8dbf1d6cd9a4b1d08f66
Bhavik Ravani <ravanibhavik@gmail.com>
2018-11-21 05:43:49 -0500

    Updated settings for static files.

--- djorg/settings.py
+++ djorg/settings.py
@@ -122,6 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
