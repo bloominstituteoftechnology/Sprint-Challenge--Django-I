6d9c9745af835142df4d89330dfdfc754021d238
Bhavik Ravani <ravanibhavik@gmail.com>
2018-11-21 05:15:26 -0500

    Updated Settings.py.

--- djorg/settings.py
+++ djorg/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
     # }
 }
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+DATABASES['default'] = dj_database_url.config(default=config('DATABASE_URL'), conn_max_age=600)
 
 
 # Password validation
