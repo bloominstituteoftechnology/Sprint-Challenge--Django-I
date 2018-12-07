7bf137d4e9116dcb8a943c2aed791a58613b62b2
Lucas Beemer <lucasbeemer@gmail.com>
2018-12-07 10:40:51 -0800

    deployed

--- djorg/urls.py
+++ djorg/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('posts')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ]
