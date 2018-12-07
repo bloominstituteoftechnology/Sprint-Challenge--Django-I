83eadf76055fee6bc533795f234ff8ec43e6fc3c
Brandon White <bwhite06@stac.edu>
2018-12-07 16:50:11 -0500

    updated files
    - ?

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
 web: gunicorn sprint_django.wsgi --log-file -
-release: python manage.py migrate
--- sprint_django/sprint_django/settings.py
+++ sprint_django/sprint_django/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-     # 'django.middleware.security.SecurityMiddleware',
+     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
   # ...
 ]
