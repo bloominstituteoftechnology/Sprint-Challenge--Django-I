a45d5bb867b945a6f3abb14db146f4c4bfa32a3f
Lucas Beemer <lucasbeemer@gmail.com>
2018-12-07 10:48:47 -0800

    import viewsets

--- djorg/urls.py
+++ djorg/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from rest_framework import routers
+from rest_framework import routers, viewsets
 
 router = routers.DefaultRouter()
 router.register('posts')
